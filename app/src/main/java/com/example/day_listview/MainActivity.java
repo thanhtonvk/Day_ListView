203428a5d537f2ee13ff3d87c922bb342766e273
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2021-02-02 21:06:25 +0700

    .

--- app/src/main/java/com/example/day_listview/MainActivity.java
+++ app/src/main/java/com/example/day_listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.day_listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,11 +23,23 @@ public class MainActivity extends AppCompatActivity {
         anhXa();
         monHocAdapter = new MonHocAdapter(MainActivity.this,R.layout.dong_monhoc,list);
         listView.setAdapter(monHocAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,list.get(position).getMoTa(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void anhXa() {
         listView = findViewById(R.id.list_item);
         list = new ArrayList<>();
+        list.add(new MonHoc("Thằng Cường","Tóc như đb quấn rẻ",R.drawable.cuong));
+        list.add(new MonHoc("Thằng Tôn","Rất đẹp trai",R.drawable.ton));
+        list.add(new MonHoc("Thằng Quang","Cha xứ nghiện ngập",R.drawable.quang));
+        list.add(new MonHoc("Thằng Cường","Tóc như đb quấn rẻ",R.drawable.cuong));
+        list.add(new MonHoc("Thằng Tôn","Rất đẹp trai",R.drawable.ton));
+        list.add(new MonHoc("Thằng Quang","Cha xứ nghiện ngập",R.drawable.quang));
         list.add(new MonHoc("Thằng Cường","Tóc như đb quấn rẻ",R.drawable.cuong));
         list.add(new MonHoc("Thằng Tôn","Rất đẹp trai",R.drawable.ton));
         list.add(new MonHoc("Thằng Quang","Cha xứ nghiện ngập",R.drawable.quang));
